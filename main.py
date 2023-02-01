e24cfc12178670a9f135b5de86c9c85ce98b9a86
Marco <marco.hauschild@convini.de>
2023-02-01 21:15:41 +0100

    Rename one variable

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ def spx_example():
 
 
 def btc_example():
-    spx500_2020_21 = get_and_save_data("BTC-USD", interval='1d', start='2020-01-01', end='2021-12-31')
-    spx500_2022 = get_and_save_data("BTC-USD", interval='1d', start='2022-01-01', end='2022-12-31')
+    btc_2020_21 = get_and_save_data("BTC-USD", interval='1d', start='2020-01-01', end='2021-12-31')
+    btc_2022 = get_and_save_data("BTC-USD", interval='1d', start='2022-01-01', end='2022-12-31')
 
-    open_2years, close_2years = calculate_all_probability_tables(spx500_2020_21, True)
+    open_2years, close_2years = calculate_all_probability_tables(btc_2020_21, True)
     open_2years.to_csv(f"output/cpd_opening_price_btc_2020_21.csv")
     close_2years.to_csv(f"output/cpd_closing_price_btc_2020_21.csv")
     visualise_prop_tables([open_2years, close_2years], ['Distribution Table for Opening Data BTC',
                                                         'Distribution Table for Close Data BTC'])
-    pred_eval = evaluate_model(spx500_2022, [open_2years, close_2years])
+    pred_eval = evaluate_model(btc_2022, [open_2years, close_2years])
     visualise_eval(pred_eval, 'Distribution of correct&wrong predictions for BTC in 2022 (trained on 2020 & 2022)',
                    True)
 
